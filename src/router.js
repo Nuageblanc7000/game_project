98981c778f52d8038c9616fb1754783509c6fac6
wetterene <wetterene.remy@gmail.com>
2023-03-06 22:46:31 +0100

    merge branch feature/formQuestion

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import FormPage from "./Pages/formPage/FormPage";
 import HomePage from "./Pages/homePage/HomePage";
+import SignUp from "./Pages/signIn/SignUp";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -14,5 +15,9 @@ export const router = createBrowserRouter([
   {
     path: "/create",
     element: <FormPage />,
+  },
+  {
+    path: "/signUp",
+    element: <SignUp />,
   },
 ]);
